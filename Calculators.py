c8400da9da96d4a6425d61a9adedc2604265262c
TheCringeLord <65627245+TheCringeLord@users.noreply.github.com>
2021-11-17 16:44:20 -0600

    fixed a bit, made it easier

--- Calculators.py
+++ Calculators.py
@@ -1,18 +1,22 @@
 System1 = int(input("Solve Mass Defect (1) or Energy/Wavelength/Freqency (2): "))
 if System1 == 1:
-    Mnucleus = (float(input("Enter Mass Number: ")) * float(1.66054E-27)) - (float(input("Enter Electron Amount: ")) * float(9.10915E-31))
-    Mprotons = (float(input("Enter Proton Amount: ")) * float(1.67262E-27)) + (float(input("Enter Neutron Amount: ")) * float(1.67495E-27))
-    print("Mnucleus =", Mnucleus), print("Mprotons =", Mprotons), print("Mass Defect =", Mnucleus - Mprotons)
+    Nucleus = (float(input("Enter Mass Number: ")) * float(1.66054E-27)) - (float(input("Enter Electron Amount: ")) * float(9.10915E-31))
+    Protons = (float(input("Enter Proton Amount: ")) * float(1.67262E-27)) + (float(input("Enter Neutron Amount: ")) * float(1.67495E-27))
+    print("Nucleus Mass =", Nucleus), print("Proton Mass =", Protons), print("Mass Defect =", Nucleus - Protons)
 if System1 == 2:
     System2 = int(input("Solve for Energy (1), Wavelength (2), or Frequency (3): "))
     if System2 == 1:
-        print("Energy = ", float(6.63E-34) * float(input("Enter Frequency (Example: 4.97E-33): ")))
+        print("Energy = ", float(6.63E-34) * float(input("Enter Frequency (Example: 4.97e-33): ")))
     if System2 == 2:
-        print("Wavelength = ", float(3E8) / float(input("Enter Frequency (Example: 4.97E-33): ")),"m")
+        print("Wavelength = ", float(3E8) / float(input("Enter Frequency (Example: 4.97e-33): ")),"m")
     if System2 == 3:
-        print("Frequency = ", float(3E8) / float(input("Enter Wavelength (Example: 4.0E-7): ")),"Hz")
+        print("Frequency = ", float(3E8) / float(input("Enter Wavelength (Example: 4.0e-7): ")),"Hz")
     if System2 > 3:
-        print("Error, No Operation Selected")
+        print("Please Select A Valid Number")
     if System2 < 1:
-        print("Error, No Operation Selected")
+        print("Please Select A Valid Number")
+if System1 < 1:
+    print("Please Select A Valid Number")
+if System1 > 2:
+    print("Please Select A Valid Number")
 input("Press Enter to Exit")
